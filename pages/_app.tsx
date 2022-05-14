1a9f1cd5b576d3fc39ca70c2a7a5028926464794
Kaito Sawada@Ozone <k.sawada@ozonehl.com>
2022-05-14 21:03:57 +0900

    add umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Script async defer src="https://analytics.incel.jp/umami.js" data-website-id="06ed1b9c-a5ed-4805-a8fb-8a7d00521b0b" />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
